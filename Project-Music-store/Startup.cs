4120a7f525030fd8bdd122f4f4b26d51374eb1a4
Abhishek <abhi_1413@yahoo.in>
2018-09-17 16:11:55 -0400

    Initial commit

---
+++ Project-Music-store/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project_Music_store.Startup))]
+namespace Project_Music_store
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>COMP 2084 ASP.NET MVC Music Store</h1>
+
+<p> This application is built in the class </p>
